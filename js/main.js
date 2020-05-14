f5833147ec2d6cdd82f7c065989a62bf6caf5929
Braxton Toomer <braxton.toomer1@gmail.com>
2020-05-14 09:16:37 -0700

    Adds JS Cached elements for grid items

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*--------------------- Constants ------------------*/
+
+
+
+
+/*--------------------- Variables ------------------*/
+
+
+
+
+/*-------------- Cached Element References --------*/
+const container = document.querySelector('#grid-container');
+
+//Create Element for Grid Items
+const items = document.createElement('div');
+items.classList.add('items');
+items.textContent = 'place holder text';
+container.appendChild(items);
+
+
+/*------------------ Event Listeners --------------*/
+
+
+
+
+
+/*--------------------- Functions -----------------*/
+
+
+
+
+
