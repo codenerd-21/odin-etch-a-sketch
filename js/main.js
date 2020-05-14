ed0febedb58aa4d795249445dfdf865adaf69185
Braxton Toomer <braxton.toomer1@gmail.com>
2020-05-14 16:55:13 -0700

    Embed event listener in init function to change background color

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 /*--------------------- Constants ------------------*/
 const START_GAME = 16;
+
 /*--------------------- Variables ------------------*/
 let count = 1;
 let cellsMax;
 
 /*-------------- Cached Element References --------*/
 const container = document.querySelector('.container');
-
-/*------------------ Event Listeners --------------*/
-//items.addEventListener('mouseover', styleEtch);
 
 /*--------------------- Functions -----------------*/
 
@@ -21,17 +19,14 @@ function init(num) {
   container.style.setProperty('grid-template-columns', 'repeat(' + num + ', 1fr)');
   for (let i = 0; i < cellsMax; i++) {
     const items = document.createElement('div');
+    //Changes background to yellow on mouseover
+    items.addEventListener('mouseover', () => {
+      items.style.backgroundColor = '#D6ED17'
+    });
     items.classList.add('items');
     items.setAttribute('id', count);
     items.textContent = 'place holder text';
     container.appendChild(items);
     count++;
   }
-}
-
-function styleEtch() {
-  let allCells = document.querySelectorAll('.items');
-  for (let i = 0; i < allCells.length; i++) {
-    allCells[i].style.backgroundColor = '#D6ED17';
-  }
-}
+};
